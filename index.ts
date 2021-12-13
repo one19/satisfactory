2c12825a98b53f309d3fb4f86c3dfe1fcf48e5fb
Drew Showalter <10665739+one19@users.noreply.github.com>
2021-12-13 20:16:40 +1100

    :sparkles: first working setup

--- index.ts
+++ index.ts
@@ -1,69 +1,90 @@
-import * as pulumi from '@pulumi/pulumi';
+// import * as pulumi from '@pulumi/pulumi';
 // import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
-
-// const eip = new aws.ec2.Eip('satisfactory-eip', {
-//   vpc: true,
-// });
 
 const sg = new awsx.ec2.SecurityGroup('satisfactory-security-group');
 awsx.ec2.SecurityGroupRule.ingress(
   'satisfactory-7777',
   sg,
   new awsx.ec2.AnyIPv4Location(),
-  new awsx.ec2.TcpPorts(7777),
+  new awsx.ec2.UdpPorts(7777),
   'allow satisfactory 7777'
 );
 awsx.ec2.SecurityGroupRule.ingress(
   'satisfactory-15000',
   sg,
   new awsx.ec2.AnyIPv4Location(),
-  new awsx.ec2.TcpPorts(15000),
+  new awsx.ec2.UdpPorts(15000),
   'allow satisfactory 15000'
 );
 awsx.ec2.SecurityGroupRule.ingress(
   'satisfactory-15777',
   sg,
   new awsx.ec2.AnyIPv4Location(),
-  new awsx.ec2.TcpPorts(15777),
+  new awsx.ec2.UdpPorts(15777),
   'allow satisfactory 15777'
 );
 awsx.ec2.SecurityGroupRule.egress(
   'all',
   sg,
   new awsx.ec2.AnyIPv4Location(),
-  new awsx.ec2.TcpPorts(0, 0),
+  new awsx.ec2.AllTraffic(),
   'allow all'
 );
-
-// const satServer = new awsx.ecs.EC2Service('satisfactory', {
-//   cluster: satCluster,
-//   taskDefinitionArgs: {
-//     container: {
-//       image: 'wolveix/satisfactory-server:latest',
-//     },
-//   },
-// });
 
 const cluster = new awsx.ecs.Cluster('satisfactory-cluster', { securityGroups: [sg] });
 
 // still failing healthchecks; are these the problem???
 const nlb = new awsx.lb.NetworkLoadBalancer('satisfactory-nlb', { external: true });
-const listener7777 = nlb.createListener('satisfactory-7777', {
+
+const target7777 = nlb.createTargetGroup('satisfactory-7777', {
   port: 7777,
+  protocol: 'UDP',
 });
-const listener15000 = nlb.createListener('satisfactory-15000', {
-  port: 15000,
-});
-const listener15777 = nlb.createListener('satisfactory-15777', {
-  port: 15777,
+const listener7777 = target7777.createListener('satisfactory-7777', {
+  port: 7777,
+  protocol: 'UDP',
 });
 
-// Define the service, building and publishing our "./app/Dockerfile", and using the load balancer.
+const target15000 = nlb.createTargetGroup('satisfactory-15000', {
+  port: 15000,
+  protocol: 'UDP',
+});
+const listener15000 = target15000.createListener('satisfactory-15000', {
+  port: 15000,
+  protocol: 'UDP',
+});
+
+const target15777 = nlb.createTargetGroup('satisfactory-15777', {
+  port: 15777,
+  protocol: 'UDP',
+});
+const listener15777 = target15777.createListener('satisfactory-15777', {
+  port: 15777,
+  protocol: 'UDP',
+});
+
 new awsx.ecs.FargateService('satisfactory', {
   cluster,
   desiredCount: 1,
   // loadBalancers: [nlb],
+  loadBalancers: [
+    {
+      targetGroupArn: target7777.targetGroup.arn,
+      containerName: 'satisfactory',
+      containerPort: 7777,
+    },
+    {
+      targetGroupArn: target15000.targetGroup.arn,
+      containerName: 'satisfactory',
+      containerPort: 15000,
+    },
+    {
+      targetGroupArn: target15777.targetGroup.arn,
+      containerName: 'satisfactory',
+      containerPort: 15777,
+    },
+  ],
   taskDefinitionArgs: {
     containers: {
       satisfactory: {
@@ -71,6 +92,7 @@ new awsx.ecs.FargateService('satisfactory', {
         memory: 8192,
         // healthCheck: {
         //   startPeriod: 300,
+        //   command: ['CMD-SHELL', 'echo hi || exit 1'],
         // },
         environment: [
           { name: 'STEAMUSER', value: 'anonymous' },
@@ -83,8 +105,5 @@ new awsx.ecs.FargateService('satisfactory', {
   },
 });
 
-// Export the URL so we can easily access it.
-// export const nlbstuff = nlb;
-export const url1 = pulumi.interpolate`http://${listener7777.endpoint.hostname}/`;
-export const url2 = pulumi.interpolate`http://${listener15000.endpoint.hostname}/`;
-export const url3 = pulumi.interpolate`http://${listener15777.endpoint.hostname}/`;
+// eslint-disable-next-line import/prefer-default-export
+export const url = listener7777.endpoint.hostname;
