30e3adc4ebe60f01c8e9befefb8fcec8fc6ba59a
Drew Showalter <10665739+one19@users.noreply.github.com>
2021-12-16 10:04:51 +1100

    :wrench: swap to nonfunctional but possible EC2 configuration.

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 // import * as pulumi from '@pulumi/pulumi';
-// import * as aws from '@pulumi/aws';
+import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
+
+// const VPCID = 'vpc-c63229a1';
+const subnetIds = ['subnet-bde023db', 'subnet-a654a9ee', 'subnet-9b9eecc3'];
 
 const sg = new awsx.ec2.SecurityGroup('satisfactory-security-group');
 awsx.ec2.SecurityGroupRule.ingress(
@@ -33,6 +36,11 @@ awsx.ec2.SecurityGroupRule.egress(
 );
 
 const cluster = new awsx.ecs.Cluster('satisfactory-cluster', { securityGroups: [sg] });
+cluster.createAutoScalingGroup('satisfactory-group', {
+  subnetIds,
+  templateParameters: { minSize: 1 },
+  launchConfigurationArgs: { instanceType: 't2.large', imageId: 'ami-0ea87b9ed0e286f93' },
+});
 
 // still failing healthchecks; are these the problem???
 const nlb = new awsx.lb.NetworkLoadBalancer('satisfactory-nlb', { external: true });
@@ -64,10 +72,23 @@ const listener15777 = target15777.createListener('satisfactory-15777', {
   protocol: 'UDP',
 });
 
-new awsx.ecs.FargateService('satisfactory', {
+const efsFs = new aws.efs.FileSystem('satisfactory-volume', {
+  availabilityZoneName: 'ap-southeast-2a',
+  encrypted: false,
+  tags: {
+    Name: 'satisfactory-config-fs',
+  },
+});
+
+// new aws.ec2.Instance('satisfactory-large', {
+//   ami: 'amzn2-ami-kernel-5.10-hvm-2.0.20211201.0-x86_64-gp2',
+//   instanceType: 't2.large',
+//   vpcSecurityGroupIds: [sg.id],
+// });
+
+new awsx.ecs.EC2Service('satisfactory', {
   cluster,
   desiredCount: 1,
-  // loadBalancers: [nlb],
   loadBalancers: [
     {
       targetGroupArn: target7777.targetGroup.arn,
@@ -86,10 +107,25 @@ new awsx.ecs.FargateService('satisfactory', {
     },
   ],
   taskDefinitionArgs: {
+    volumes: [
+      {
+        name: 'service-storage',
+        dockerVolumeConfiguration: {
+          scope: 'shared',
+          autoprovision: true,
+          driver: 'local',
+          driverOpts: {
+            type: 'nfs',
+            device: `${efsFs.dnsName}:/`,
+            o: `addr=${efsFs.dnsName},rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport`,
+          },
+        },
+      },
+    ],
     containers: {
       satisfactory: {
         image: 'wolveix/satisfactory-server:latest',
-        memory: 8192,
+        memory: 7000,
         // healthCheck: {
         //   startPeriod: 300,
         //   command: ['CMD-SHELL', 'echo hi || exit 1'],
@@ -98,6 +134,13 @@ new awsx.ecs.FargateService('satisfactory', {
           { name: 'STEAMUSER', value: 'anonymous' },
           { name: 'STEAMBETA', value: 'false' },
           { name: 'MAXBACKUPS', value: '10' },
+        ],
+        mountPoints: [
+          {
+            containerPath: '/config',
+            readOnly: false,
+            sourceVolume: 'service-storage',
+          },
         ],
         portMappings: [listener7777, listener15000, listener15777],
       },
